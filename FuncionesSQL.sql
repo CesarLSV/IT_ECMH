d20904b86c2165c6f57b957eecc1ea2c45ce71cd
CesarLSV <cesarlopez1909@gmail.com>
2018-05-31 16:01:35 -0600

    s

--- FuncionesSQL.sql
+++ FuncionesSQL.sql
@@ -10,3 +10,26 @@ WHEN datepart(month, ''+@bd+'') = datepart(month, getdate())
                          AND datepart(day, ''+@bd+'') > datepart(day, getdate()) THEN 1 ELSE 0 END) AS AGE
 FROM            dbo.PEOPLE
 WHERE        (PEOPLE_CODE_ID = 'P019950004')
+
+
+
+
+
+/*Crear Json desde SQL
+Unicamente SQL 2016 o superior*/
+
+
+SELECT     modulo.modulo, modulo.descripcion,
+(
+SELECT m.modulo, m.descripcion, r.descripcion, r.nombrereporte
+
+            FROM reporte_modulo as m,reporte_modulo  as r
+
+            WHERE m.modulo = r.modulo 
+
+            FOR JSON auto
+			
+			) AS Emails
+
+FROM         modulo
+
